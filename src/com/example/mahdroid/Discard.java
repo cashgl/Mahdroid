d34fbdcaaa66d8060fa78b4ce96d3df4b22090e1
Grayson Cash <cashgl@plu.edu>
2014-03-04 17:13:30 -0800

    Implemented methods for Discard

--- src/com/example/mahdroid/Discard.java
+++ src/com/example/mahdroid/Discard.java
@@ -9,16 +9,30 @@ public class Discard {
 	private int size;
 	
 	public Discard() {
-		// TODO Auto-generated constructor stub
+		this.pile = new Stack<Tile>();
+		this.size = 0;
 	}
 	
-	public void add(){
-		
-		
+	public void add(Tile tile){
+		this.pile.push(tile);
+		this.size++;
 	}
 
 	public Tile remove(Tile tile){
+		this.size--;
+		return pile.pop();
+	}
+	
+	public int getSize() {
+		return this.size;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < this.size; i++) {
+			sb.append(this.pile.get(i) + "\n");
+		}
+		return sb.toString();
 		
-		return tile;
 	}
 }
