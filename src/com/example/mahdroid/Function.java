1ac29522792a68679204056fd83c03573c2c2e60
migaowu <wuma@plu.edu>
2014-03-05 10:34:52 -0800

    Modified Function

--- src/com/example/mahdroid/Function.java
+++ src/com/example/mahdroid/Function.java
@@ -3,38 +3,45 @@ package com.example.mahdroid;
 public class Function {
 
 	public static boolean eat(Hand h){
-		
-		return true;
-	}
-	
-	public static boolean dou(Hand h){
-		int i = 0;
-		boolean canDouble = false;
-		while (i < h.getSize() - 1 && !canDouble && h.getSize() > 1) {
-			if (h.tileAt(i).equals(h.tileAt(i+1)))
+		for (int i = 0; i< h.getSize() - 2; i++) {
+			if (h.tileAt(i).getValue() == h.tileAt(i+1).getValue() && h.tileAt(i+1).getValue() == h.tileAt(i+2).getValue())
 				return true;
-			i++;
 		}
 		return false;
 	}
-	
+
+	public static boolean dou(Hand h){
+		for (int i = 0; i< h.getSize() - 2; i++) {
+			if (h.tileAt(i).equals(h.tileAt(i+1)))
+				return true;
+		}
+		return false;
+	}
+
 	public static boolean triple(Hand h){
 		for (int i = 0; i< h.getSize() - 2; i++) {
 			if (h.tileAt(i).equals(h.tileAt(i+2)))
-					return true;
+				return true;
 		}
 		return false;
 	}
-	
+
 	public static boolean win(Hand h){
-		
-		
-		return true;
+		if (h.getSize() == 2 && dou(h))
+			return true;
+		else if (h.getSize() >= 5 && dou(h) && eat(h))
+			return true;
+		else if (h.getSize() >= 5 && dou(h) && triple(h))
+			return true;
+		else if (h.getSize() >= 8 && dou(h) && eat(h) && triple(h))
+			return true;
+		else 
+			return false;
 	}
-	
+
 	public static boolean skip(Hand h){
-		
+
 		return true;
 	}
-	
+
 }
