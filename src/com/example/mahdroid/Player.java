f865af5e887024636c29f4b10be72f07e9050c6a
Grayson Cash <cashgl@plu.edu>
2014-03-18 11:45:26 -0700

    Added functionality to Evaluate, DiscardTile, and CallFunctions
    Returning everything it’s available for as a String
    CallFunction now calls to correct function to manipulate hand

--- src/com/example/mahdroid/Player.java
+++ src/com/example/mahdroid/Player.java
@@ -17,7 +17,7 @@ public class Player {
 	}
 	
 	public Tile drawTile(){
-		
+		tempTile = deck.draw();
 		return tempTile;
 	}
 	
@@ -25,34 +25,46 @@ public class Player {
 		return discard.grabLast();
 	}
 	
-	
 	/**
 	 * Takes the index of the hand and discards that tile
 	 * @param i
 	 */
 	public void discardTile(int i){
-		if (i >= 0 && i < 13)
+		if (i >= 0 && i < 13) {
 			discard.add(hand.removeAt(i));
+			hand.add(tempTile);
+			tempTile = null;
+		}
 		else {
 			discard.add(tempTile);
 			tempTile = null;
 		}
 	}
 	
-	public void evaluate(){
+	public String evaluate(){
+		StringBuilder s = new StringBuilder();
+		if (Function.eat(hand, tempTile))
+			s.append("e");
+		if (Function.dou(hand, tempTile))
+			s.append("d");
+		if (Function.triple(hand, tempTile))
+			s.append("t");
+		if (Function.skip(hand))
+			s.append("s");
+		if (Function.win(hand, tempTile))
+			s.append("w");
 		
-		
+		return s.toString();
 	}
 	
 	public boolean callFunction(String function){
-		if (function.contains("e"))
-			return Function.eat(hand, tempTile);
-		else if (function.contains("d"))
-			return Function.dou(hand, tempTile);
-		else if (function.contains("t"))
-			return Function.triple(hand, tempTile);
-		else if (function.contains("w"))
-			return Function.win(hand, tempTile);
+		if (function.equals("e"))
+			Function.performEat(hand, tempTile);
+	/*	else if (function.equals("d"))
+			Function.performDouble(hand, tempTile);
+		else if (function.equals("t"))
+			Function.performTriple(hand, tempTile);  */
+
 		return false;
 	}
 }
