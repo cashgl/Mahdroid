78dbad4516257502e77f089facffd6577a5d9610
Grayson Cash <cashgl@plu.edu>
2014-03-19 22:28:02 -0700

    tiny change

--- src/com/example/mahdroid/Instructions.java
+++ src/com/example/mahdroid/Instructions.java
@@ -1,8 +1,7 @@
 package com.example.mahdroid;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 import android.webkit.WebView;
 
 public class Instructions extends Activity {
