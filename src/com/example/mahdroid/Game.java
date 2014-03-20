11538d3042d658cc7829c0bd415dcb79bd3a7475
Grayson Cash <cashgl@plu.edu>
2014-03-19 23:11:12 -0700

    Removed "setupStuff"

--- src/com/example/mahdroid/Game.java
+++ src/com/example/mahdroid/Game.java
@@ -20,10 +20,8 @@ import android.widget.TextView;
 public class Game extends Activity {
 
 	EditText suitField, valueField;
-	TextView txt;
 	Hand hand;
 	ArrayList<Button> buttons;
-	/////
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +33,8 @@ public class Game extends Activity {
 			buttons.add((Button)findViewById(i));
 		}
 		for (int i = 0; i <= 13; i++) {
-			buttons.get(i).setOnClickListener(myListener);		
+			buttons.get(i).setOnClickListener(colorListener);		
 		}
-		
 		
 		Button eatButton = (Button) findViewById(R.id.eatButton);
 		eatButton.setOnTouchListener(functionOnTouch);
@@ -50,114 +47,10 @@ public class Game extends Activity {
 		
 		Button winButton = (Button) findViewById(R.id.winButton);
 		winButton.setOnTouchListener(functionOnTouch);
-		
-		//setupStuff();
 
 	}
-
-	public void setupStuff() {
-		txt = (TextView) findViewById(R.id.textView1);
-		txt.setMovementMethod(new ScrollingMovementMethod());
-		
-		//Button addButton = (Button) findViewById(R.id.addButton);
-		//suitField = (EditText) findViewById(R.id.suitField);
-		//valueField = (EditText) findViewById(R.id.valueField);
-
-		/*addButton.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				if(suitField.getText().length()==0){
-                    suitField.setError("Field cannot be left blank.");
-                }
-				else if (valueField.getText().length()==0)
-					valueField.setError("Field cannot be left blank.");
-				else {
-					int suit = Integer.parseInt(suitField.getText().toString());
-					int value = Integer.parseInt(valueField.getText().toString());
-
-					Tile temp = new Tile(suit,value);
-					hand.add(temp);
-					txt.setText("\n" + hand + "\n");
-				}
-			}
-		});*/
-
-		hand = new Hand();
-		Tile tile111 = new Tile(1, 1);
-		Tile tile112 = new Tile(3, 5);
-		Tile tile113 = new Tile(1, 2);
-		Tile tile14 = new Tile(2, 3);
-		Tile tile15 = new Tile(1, 2);
-		Tile tile16 = new Tile(2, 2);
-		Tile tile17 = new Tile(2, 4);
-
-		hand.add(tile111);
-		hand.add(tile112);
-		hand.add(tile113);
-		hand.add(tile14);
-		hand.add(tile15);
-		hand.add(tile16);
-		hand.add(tile17);
-		Tile tile18 = new Tile(1, 2);
-		hand.add(tile18);
-		txt.append(hand.toString());
-
-
-
-		Tile t = new Tile(1,2);
-		if (Function.triple(hand, t))
-			txt.append("true");
-		else
-			txt.append("false");
-		txt.append("\n");
-
-		hand.remove(tile113);
-		if (Function.triple(hand, t))
-			txt.append("true\n");
-		else
-			txt.append("false\n");
-		txt.append("\n");
-
-		Deck deck = new Deck();
-		txt.append(deck.toString());
-
-		ArrayList<Button> tilesInHand = new ArrayList<Button>();
-		Button tile0 = (Button) findViewById(R.id.playerTile0);
-		tilesInHand.add(tile0);
-		Button tile1 = (Button) findViewById(R.id.playerTile1);
-		tilesInHand.add(tile1);
-		Button tile2 = (Button) findViewById(R.id.playerTile2);
-		tilesInHand.add(tile2);
-		Button tile3 = (Button) findViewById(R.id.playerTile3);
-		tilesInHand.add(tile3);
-		Button tile4 = (Button) findViewById(R.id.playerTile4);
-		tilesInHand.add(tile4);
-		Button tile5 = (Button) findViewById(R.id.playerTile5);
-		tilesInHand.add(tile5);
-		Button tile6 = (Button) findViewById(R.id.playerTile6);
-		tilesInHand.add(tile6);
-		Button tile7 = (Button) findViewById(R.id.playerTile7);
-		tilesInHand.add(tile7);
-		Button tile8 = (Button) findViewById(R.id.playerTile8);
-		tilesInHand.add(tile8);
-		Button tile9 = (Button) findViewById(R.id.playerTile9);
-		tilesInHand.add(tile9);
-		Button tile10 = (Button) findViewById(R.id.playerTile10);
-		tilesInHand.add(tile10);
-		Button tile11 = (Button) findViewById(R.id.playerTile11);
-		tilesInHand.add(tile11);
-		Button tile12 = (Button) findViewById(R.id.playerTile12);
-		tilesInHand.add(tile12);
-
-		deck = new Deck();
-		for (int i = 0; i<tilesInHand.size(); i++) {
-			Tile ti = deck.draw();
-			tilesInHand.get(i).setText(ti.getSuit() + "\n" + ti.getValue());
-		}
-
-	}
-	private OnClickListener myListener = new OnClickListener() {
+	
+	private OnClickListener colorListener = new OnClickListener() {
 		
 		@Override
 		public void onClick(View v) {
