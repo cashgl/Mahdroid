0b164761026c78de459ff96746be06aca16bb77e
Grayson Cash <cashgl@plu.edu>
2014-04-01 15:39:19 -0700

    Restructuring of Game class

--- src/com/example/mahdroid/Game.java
+++ src/com/example/mahdroid/Game.java
@@ -1,21 +1,19 @@
 package com.example.mahdroid;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class Game extends Activity {
 
@@ -24,6 +22,7 @@ public class Game extends Activity {
 	ArrayList<Button> buttons;
 	Deck deck;
 	Hand playerHand;
+	int currentRound, currentPlayer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,43 +31,12 @@ public class Game extends Activity {
 		
 		deck = new Deck();
 		playerHand = new Hand();
+		
+		currentPlayer = randomPlayer();
 
-		buttons = new ArrayList<Button>();
-		for (int i = 0x7f090001; i <= 0x7f09000e; i++) {
-			buttons.add((Button)findViewById(i));
-		}
-		for (int i = 0; i <= 13; i++) {
-			playerHand.add(deck.draw());
-		}
-		for (int i = 0; i <= 12; i++) {
-			Tile tempT = playerHand.tileAt(i);
-			Button tempB = buttons.get(i);
-			if (tempT.getSuit() == 0) 
-				tempB.setBackgroundColor(Color.CYAN);
-			else if (tempT.getSuit() == 1)
-				tempB.setBackgroundColor(Color.YELLOW);
-			else if (tempT.getSuit() == 2)
-				tempB.setBackgroundColor(Color.GREEN);
-			else if (tempT.getSuit() == 3)
-				tempB.setBackgroundColor(Color.RED);
-			else if (tempT.getSuit() == 4)
-				tempB.setBackgroundColor(Color.GRAY);
-			tempB.setText("" + tempT.getValue());
-			tempB.setOnClickListener(suit_valueListener);
-			//buttons.get(i).setOnClickListener(colorListener);		
-		}
-		
-		Button eatButton = (Button) findViewById(R.id.eatButton);
-		eatButton.setOnTouchListener(functionOnTouch);
-		
-		Button doubleButton = (Button) findViewById(R.id.doubleButton);
-		doubleButton.setOnTouchListener(functionOnTouch);
-		
-		Button tripleButton = (Button) findViewById(R.id.tripleButton);
-		tripleButton.setOnTouchListener(functionOnTouch);
-		
-		Button winButton = (Button) findViewById(R.id.winButton);
-		winButton.setOnTouchListener(functionOnTouch);
+		//Fix this method so that it distributes 
+		//cards one by one to each player
+		createHands();
 
 	}
 	
@@ -124,5 +92,49 @@ public class Game extends Activity {
 			return true;
 		}
 	};
+	
+	private void createHands() {
+		buttons = new ArrayList<Button>();
+		for (int i = 0x7f090001; i <= 0x7f09000e; i++) {
+			buttons.add((Button)findViewById(i));
+		}
+		for (int i = 0; i <= 13; i++) {
+			playerHand.add(deck.draw());
+		}
+		for (int i = 0; i <= 12; i++) {
+			Tile tempT = playerHand.tileAt(i);
+			Button tempB = buttons.get(i);
+			if (tempT.getSuit() == 0) 
+				tempB.setBackgroundColor(Color.CYAN);
+			else if (tempT.getSuit() == 1)
+				tempB.setBackgroundColor(Color.YELLOW);
+			else if (tempT.getSuit() == 2)
+				tempB.setBackgroundColor(Color.GREEN);
+			else if (tempT.getSuit() == 3)
+				tempB.setBackgroundColor(Color.RED);
+			else if (tempT.getSuit() == 4)
+				tempB.setBackgroundColor(Color.GRAY);
+			tempB.setText("" + tempT.getValue());
+			tempB.setOnClickListener(suit_valueListener);
+			//buttons.get(i).setOnClickListener(colorListener);		
+		}
+		
+		Button eatButton = (Button) findViewById(R.id.eatButton);
+		eatButton.setOnTouchListener(functionOnTouch);
+		
+		Button doubleButton = (Button) findViewById(R.id.doubleButton);
+		doubleButton.setOnTouchListener(functionOnTouch);
+		
+		Button tripleButton = (Button) findViewById(R.id.tripleButton);
+		tripleButton.setOnTouchListener(functionOnTouch);
+		
+		Button winButton = (Button) findViewById(R.id.winButton);
+		winButton.setOnTouchListener(functionOnTouch);
+	}
+	
+	private int randomPlayer() {
+		Random r = new Random();
+		return r.nextInt(4);
+	}
 
 }
