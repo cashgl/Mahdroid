a21bf61e5db41ecfc1017c799ba62df21809fee5
Grayson Cash <cashgl@plu.edu>
2014-04-02 02:37:42 -0700

    Added icon, fixed listener
    We can’t keep the icon forever, it is just temporary since it’s not ours
    Fixed OnClickListener for tiles

--- src/com/example/mahdroid/Game.java
+++ src/com/example/mahdroid/Game.java
@@ -40,85 +40,85 @@ public class Game extends Activity {
 		//Fix this method so that it distributes 
 		//cards one by one to each player
 		createHands();
-		
+
 		Button temp = (Button) findViewById(R.id.bot2Discard_Button2);
-		
+
 		LayoutParams botDiscard1Params = (LayoutParams) findViewById(R.id.botDiscard1).getLayoutParams(),
 				botDiscard2Params = (LayoutParams) findViewById(R.id.botDiscard2).getLayoutParams(),
 				playerDiscardParams = (LayoutParams) findViewById(R.id.playerDiscard).getLayoutParams();
-		
+
 		int discardHeight = botDiscard1Params.height;
 		temp.setText("" + discardHeight);
 		botDiscard2Params.height = discardHeight;
 		playerDiscardParams.height = discardHeight;
 	}
-	
+
 	private void createHands() {
-			//Associates the buttons to a player's hand
-			associateHands();
-	
-			//Instantiates the players' hands
-			playerHand = new Hand();
-			bot1Hand = new Hand();
-			bot2Hand = new Hand();
-			bot3Hand = new Hand();
-			
-			//Deals the tiles to each player
-			for (int i = 0; i <= 12; i++) {
-				playerHand.add(deck.draw());
-				bot1Hand.add(deck.draw());
-				bot2Hand.add(deck.draw());
-				bot3Hand.add(deck.draw());
-			}
-	
-			//This outer loop controls the deal for each player
-	//////////This won't stay since we won't be seeing other players' hands
-			for (int k = 0; k <= 3; k++) {
-				Tile tempT;
-				Button tempB;
-				for (int i = 0; i <= 12; i++) {
-					if (k == 0) {
-						tempT = playerHand.tileAt(i);
-						tempB = playerButtons.get(i);
-					} else if (k == 1) {
-						tempT = bot1Hand.tileAt(i);
-						tempB = bot1Buttons.get(i);
-					} else if (k == 2) {
-						tempT = bot2Hand.tileAt(i);
-						tempB = bot2Buttons.get(i);
-					} else {
-						tempT = bot3Hand.tileAt(i);
-						tempB = bot3Buttons.get(i);
-					}
-					if (tempT.getSuit() == 0) 
-						tempB.setBackgroundColor(Color.CYAN);
-					else if (tempT.getSuit() == 1)
-						tempB.setBackgroundColor(Color.YELLOW);
-					else if (tempT.getSuit() == 2)
-						tempB.setBackgroundColor(Color.GREEN);
-					else if (tempT.getSuit() == 3)
-						tempB.setBackgroundColor(Color.RED);
-					else if (tempT.getSuit() == 4)
-						tempB.setBackgroundColor(Color.GRAY);
-					tempB.setText("" + tempT.getValue());
-					tempB.setOnClickListener(suit_valueListener);
-					//buttons.get(i).setOnClickListener(colorListener);	
-				}
-			}
-	
-			//Creates the function buttons and associates their action listener
-			Button eatButton = (Button) findViewById(R.id.eatButton);
-			eatButton.setOnTouchListener(functionOnTouch);
-	
-			Button doubleButton = (Button) findViewById(R.id.doubleButton);
-			doubleButton.setOnTouchListener(functionOnTouch);
-	
-			Button tripleButton = (Button) findViewById(R.id.tripleButton);
-			tripleButton.setOnTouchListener(functionOnTouch);
-	
-			Button winButton = (Button) findViewById(R.id.winButton);
-			winButton.setOnTouchListener(functionOnTouch);
+		//Associates the buttons to a player's hand
+		associateHands();
+
+		//Instantiates the players' hands
+		playerHand = new Hand();
+		bot1Hand = new Hand();
+		bot2Hand = new Hand();
+		bot3Hand = new Hand();
+
+		//Deals the tiles to each player
+		for (int i = 0; i <= 12; i++) {
+			playerHand.add(deck.draw());
+			bot1Hand.add(deck.draw());
+			bot2Hand.add(deck.draw());
+			bot3Hand.add(deck.draw());
 		}
+
+		//This outer loop controls the deal for each player
+		//////////This won't stay since we won't be seeing other players' hands
+		for (int k = 0; k <= 3; k++) {
+			Tile tempT;
+			Button tempB;
+			for (int i = 0; i <= 12; i++) {
+				if (k == 0) {
+					tempT = playerHand.tileAt(i);
+					tempB = playerButtons.get(i);
+				} else if (k == 1) {
+					tempT = bot1Hand.tileAt(i);
+					tempB = bot1Buttons.get(i);
+				} else if (k == 2) {
+					tempT = bot2Hand.tileAt(i);
+					tempB = bot2Buttons.get(i);
+				} else {
+					tempT = bot3Hand.tileAt(i);
+					tempB = bot3Buttons.get(i);
+				}
+				if (tempT.getSuit() == 0) 
+					tempB.setBackgroundColor(Color.CYAN);
+				else if (tempT.getSuit() == 1)
+					tempB.setBackgroundColor(Color.YELLOW);
+				else if (tempT.getSuit() == 2)
+					tempB.setBackgroundColor(Color.GREEN);
+				else if (tempT.getSuit() == 3)
+					tempB.setBackgroundColor(Color.RED);
+				else if (tempT.getSuit() == 4)
+					tempB.setBackgroundColor(Color.GRAY);
+				tempB.setText("" + tempT.getValue());
+				tempB.setOnClickListener(new SuitValueListener(tempT.getSuit(),tempT.getValue()));
+				//buttons.get(i).setOnClickListener(colorListener);	
+			}
+		}
+
+		//Creates the function buttons and associates their action listener
+		Button eatButton = (Button) findViewById(R.id.eatButton);
+		eatButton.setOnTouchListener(functionOnTouch);
+
+		Button doubleButton = (Button) findViewById(R.id.doubleButton);
+		doubleButton.setOnTouchListener(functionOnTouch);
+
+		Button tripleButton = (Button) findViewById(R.id.tripleButton);
+		tripleButton.setOnTouchListener(functionOnTouch);
+
+		Button winButton = (Button) findViewById(R.id.winButton);
+		winButton.setOnTouchListener(functionOnTouch);
+	}
 
 	private int randomPlayer() {
 		Random r = new Random();
@@ -140,7 +140,7 @@ public class Game extends Activity {
 		playerButtons.add((Button)findViewById(R.id.playerTile10));
 		playerButtons.add((Button)findViewById(R.id.playerTile11));
 		playerButtons.add((Button)findViewById(R.id.playerTile12));
-	
+
 		bot1Buttons = new ArrayList<Button>();
 		bot1Buttons.add((Button)findViewById(R.id.botTile1_0));
 		bot1Buttons.add((Button)findViewById(R.id.botTile1_1));
@@ -155,7 +155,7 @@ public class Game extends Activity {
 		bot1Buttons.add((Button)findViewById(R.id.botTile1_10));
 		bot1Buttons.add((Button)findViewById(R.id.botTile1_11));
 		bot1Buttons.add((Button)findViewById(R.id.botTile1_12));
-	
+
 		bot2Buttons = new ArrayList<Button>();
 		bot2Buttons.add((Button)findViewById(R.id.botTile2_0));
 		bot2Buttons.add((Button)findViewById(R.id.botTile2_1));
@@ -170,7 +170,7 @@ public class Game extends Activity {
 		bot2Buttons.add((Button)findViewById(R.id.botTile2_10));
 		bot2Buttons.add((Button)findViewById(R.id.botTile2_11));
 		bot2Buttons.add((Button)findViewById(R.id.botTile2_12));
-	
+
 		bot3Buttons = new ArrayList<Button>();
 		bot3Buttons.add((Button)findViewById(R.id.botTile3_0));
 		bot3Buttons.add((Button)findViewById(R.id.botTile3_1));
@@ -190,37 +190,42 @@ public class Game extends Activity {
 	@Override
 	public void onBackPressed() {
 		//This method confirms if the user wants to exit the activity
-	    new AlertDialog.Builder(this)
-	        .setIcon(android.R.drawable.ic_dialog_alert)
-	        .setTitle("Quitting Game")
-	        .setMessage("Are you sure you want to quit the game?\nThis will erase all progress")
-	        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-	        @Override
-	        public void onClick(DialogInterface dialog, int which) {
-	            finish();    
-	        }
-	    })
-	    .setNegativeButton("No", null)
-	    .setCancelable(false)
-	    .show();
+		new AlertDialog.Builder(this)
+		.setIcon(android.R.drawable.ic_dialog_alert)
+		.setTitle("Quitting Game")
+		.setMessage("Are you sure you want to quit the game?\nThis will erase all progress")
+		.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				finish();    
+			}
+		})
+		.setNegativeButton("No", null)
+		.setCancelable(false)
+		.show();
 	}
 
-	private OnClickListener suit_valueListener = new OnClickListener() {
+	public class SuitValueListener implements OnClickListener {
+		int suit, value;
+
+		public SuitValueListener(int s, int v) {
+			suit = s;
+			value = v;
+		}
 
 		@Override
 		public void onClick(View v) {
-			Button temp = (Button) v;
-			int j = playerButtons.indexOf(temp);
 			AlertDialog.Builder builder = new AlertDialog.Builder(Game.this);
-			builder.setTitle("Suit: " + playerHand.tileAt(j).getSuit() + 
-					", Value: " + playerHand.tileAt(j).getValue());
+			builder.setTitle("Suit: " + suit + 
+					", Value: " + value);
 			builder.setPositiveButton("OK", null);
 
 			AlertDialog ad = builder.create();
 			ad.show();
 
 		}
-	};
+
+	}
 
 	private OnTouchListener functionOnTouch = new OnTouchListener() {
 
