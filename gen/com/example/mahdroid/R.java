1f9007d85ea7b2044beb86b171b87d2fbd5d2d38
Grayson Cash <cashgl@plu.edu>
2014-04-02 00:46:28 -0700

    A LOT of interface changes!
    Mostly tweaks and a lot of generalization
    Added styles to buttons instead of hard coding
    Created dividers between sections

--- gen/com/example/mahdroid/R.java
+++ gen/com/example/mahdroid/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int black=0x7f06000b;
         public static final int blue=0x7f060003;
         public static final int chartreuse3=0x7f060006;
         public static final int light_blue=0x7f060007;
@@ -37,77 +38,85 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090046;
-        public static final int botDiscard1=0x7f09003d;
-        public static final int botDiscard2=0x7f09003b;
-        public static final int botDiscard3=0x7f09003f;
-        public static final int botHand1=0x7f09000f;
-        public static final int botHand2=0x7f09001a;
-        public static final int botHand3=0x7f090028;
-        public static final int botTile1_0=0x7f090010;
-        public static final int botTile1_1=0x7f090011;
-        public static final int botTile1_2=0x7f090012;
-        public static final int botTile1_3=0x7f090013;
-        public static final int botTile1_4=0x7f090014;
-        public static final int botTile1_5=0x7f090015;
-        public static final int botTile1_6=0x7f090016;
-        public static final int botTile1_7=0x7f090017;
-        public static final int botTile1_8=0x7f090018;
-        public static final int botTile1_9=0x7f090019;
-        public static final int botTile2_0=0x7f09001b;
-        public static final int botTile2_1=0x7f09001c;
-        public static final int botTile2_10=0x7f090025;
-        public static final int botTile2_11=0x7f090026;
-        public static final int botTile2_12=0x7f090027;
-        public static final int botTile2_2=0x7f09001d;
-        public static final int botTile2_3=0x7f09001e;
-        public static final int botTile2_4=0x7f09001f;
-        public static final int botTile2_5=0x7f090020;
-        public static final int botTile2_6=0x7f090021;
-        public static final int botTile2_7=0x7f090022;
-        public static final int botTile2_8=0x7f090023;
-        public static final int botTile2_9=0x7f090024;
-        public static final int botTile3_0=0x7f090029;
-        public static final int botTile3_1=0x7f09002a;
-        public static final int botTile3_2=0x7f09002b;
-        public static final int botTile3_3=0x7f09002c;
-        public static final int botTile3_4=0x7f09002d;
-        public static final int botTile3_5=0x7f09002e;
-        public static final int botTile3_6=0x7f09002f;
-        public static final int botTile3_7=0x7f090030;
-        public static final int botTile3_8=0x7f090031;
-        public static final int botTile3_9=0x7f090032;
-        public static final int button1=0x7f09003e;
-        public static final int button2=0x7f090040;
-        public static final int button3=0x7f09003c;
-        public static final int button4=0x7f090041;
-        public static final int discards=0x7f090039;
-        public static final int doubleButton=0x7f090036;
-        public static final int eatButton=0x7f090035;
-        public static final int functionButtons=0x7f090033;
-        public static final int goToInstructions=0x7f090045;
-        public static final int playerDiscard=0x7f09003a;
+        public static final int action_settings=0x7f09004e;
+        public static final int botDiscard1=0x7f090045;
+        public static final int botDiscard2=0x7f090043;
+        public static final int botDiscard3=0x7f090046;
+        public static final int botHand1=0x7f090010;
+        public static final int botHand2=0x7f09001f;
+        public static final int botHand3=0x7f09002d;
+        public static final int botTile1_0=0x7f090011;
+        public static final int botTile1_1=0x7f090012;
+        public static final int botTile1_10=0x7f09001b;
+        public static final int botTile1_11=0x7f09001c;
+        public static final int botTile1_12=0x7f09001d;
+        public static final int botTile1_2=0x7f090013;
+        public static final int botTile1_3=0x7f090014;
+        public static final int botTile1_4=0x7f090015;
+        public static final int botTile1_5=0x7f090016;
+        public static final int botTile1_6=0x7f090017;
+        public static final int botTile1_7=0x7f090018;
+        public static final int botTile1_8=0x7f090019;
+        public static final int botTile1_9=0x7f09001a;
+        public static final int botTile2_0=0x7f090020;
+        public static final int botTile2_1=0x7f090021;
+        public static final int botTile2_10=0x7f09002a;
+        public static final int botTile2_11=0x7f09002b;
+        public static final int botTile2_12=0x7f09002c;
+        public static final int botTile2_2=0x7f090022;
+        public static final int botTile2_3=0x7f090023;
+        public static final int botTile2_4=0x7f090024;
+        public static final int botTile2_5=0x7f090025;
+        public static final int botTile2_6=0x7f090026;
+        public static final int botTile2_7=0x7f090027;
+        public static final int botTile2_8=0x7f090028;
+        public static final int botTile2_9=0x7f090029;
+        public static final int botTile3_0=0x7f09002e;
+        public static final int botTile3_1=0x7f09002f;
+        public static final int botTile3_10=0x7f090038;
+        public static final int botTile3_11=0x7f090039;
+        public static final int botTile3_12=0x7f09003a;
+        public static final int botTile3_2=0x7f090030;
+        public static final int botTile3_3=0x7f090031;
+        public static final int botTile3_4=0x7f090032;
+        public static final int botTile3_5=0x7f090033;
+        public static final int botTile3_6=0x7f090034;
+        public static final int botTile3_7=0x7f090035;
+        public static final int botTile3_8=0x7f090036;
+        public static final int botTile3_9=0x7f090037;
+        public static final int button1=0x7f090048;
+        public static final int button2=0x7f090047;
+        public static final int button3=0x7f090044;
+        public static final int button4=0x7f090049;
+        public static final int discards=0x7f090041;
+        public static final int doubleButton=0x7f09003e;
+        public static final int eatButton=0x7f09003d;
+        public static final int functionButtons=0x7f09003b;
+        public static final int goToInstructions=0x7f09004d;
+        public static final int lineBot1=0x7f09001e;
+        public static final int lineBot3=0x7f090001;
+        public static final int playerDiscard=0x7f090042;
         public static final int playerHand=0x7f090000;
-        public static final int playerTile0=0x7f090001;
-        public static final int playerTile1=0x7f090002;
-        public static final int playerTile10=0x7f09000b;
-        public static final int playerTile11=0x7f09000c;
-        public static final int playerTile12=0x7f09000d;
-        public static final int playerTile2=0x7f090003;
-        public static final int playerTile3=0x7f090004;
-        public static final int playerTile4=0x7f090005;
-        public static final int playerTile5=0x7f090006;
-        public static final int playerTile6=0x7f090007;
-        public static final int playerTile7=0x7f090008;
-        public static final int playerTile8=0x7f090009;
-        public static final int playerTile9=0x7f09000a;
-        public static final int playerTileTemp=0x7f09000e;
-        public static final int skipButton=0x7f090034;
-        public static final int startGame=0x7f090044;
-        public static final int textView1=0x7f090043;
-        public static final int tripleButton=0x7f090037;
-        public static final int webView1=0x7f090042;
-        public static final int winButton=0x7f090038;
+        public static final int playerTile0=0x7f090002;
+        public static final int playerTile1=0x7f090003;
+        public static final int playerTile10=0x7f09000c;
+        public static final int playerTile11=0x7f09000d;
+        public static final int playerTile12=0x7f09000e;
+        public static final int playerTile2=0x7f090004;
+        public static final int playerTile3=0x7f090005;
+        public static final int playerTile4=0x7f090006;
+        public static final int playerTile5=0x7f090007;
+        public static final int playerTile6=0x7f090008;
+        public static final int playerTile7=0x7f090009;
+        public static final int playerTile8=0x7f09000a;
+        public static final int playerTile9=0x7f09000b;
+        public static final int playerTileTemp=0x7f09000f;
+        public static final int skipButton=0x7f09003c;
+        public static final int startGame=0x7f09004c;
+        public static final int textView1=0x7f09004b;
+        public static final int tripleButton=0x7f09003f;
+        public static final int webView1=0x7f09004a;
+        public static final int winButton=0x7f090040;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
@@ -153,10 +162,15 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f070005;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f070006;
+        public static final int bot2Button=0x7f070002;
+        public static final int line_horiz=0x7f070004;
+        public static final int line_vert=0x7f070003;
+        public static final int sideButton=0x7f070001;
+        public static final int sideHand=0x7f070000;
     }
 }
--- src/com/example/mahdroid/Game.java
+++ src/com/example/mahdroid/Game.java
@@ -21,7 +21,7 @@ public class Game extends Activity {
 
 	EditText suitField, valueField;
 	Hand hand;
-	ArrayList<Button> playerButtons;
+	ArrayList<Button> playerButtons, bot3Tiles;
 	Deck deck;
 	Hand playerHand;
 	int currentRound, currentPlayer;
@@ -30,10 +30,10 @@ public class Game extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
-		
+
 		deck = new Deck();
 		playerHand = new Hand();
-		
+
 		currentPlayer = randomPlayer();
 
 		//Fix this method so that it distributes 
@@ -41,67 +41,78 @@ public class Game extends Activity {
 		createHands();
 
 	}
-	
+
 	private OnClickListener suit_valueListener = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View v) {
 			Button temp = (Button) v;
 			int j = playerButtons.indexOf(temp);
 			AlertDialog.Builder builder = new AlertDialog.Builder(Game.this);
-	        builder.setTitle("Suit: " + playerHand.tileAt(j).getSuit() + 
-	        		", Value: " + playerHand.tileAt(j).getValue());
-	        builder.setPositiveButton("OK", null);
+			builder.setTitle("Suit: " + playerHand.tileAt(j).getSuit() + 
+					", Value: " + playerHand.tileAt(j).getValue());
+			builder.setPositiveButton("OK", null);
 
-	        AlertDialog ad = builder.create();
-	        ad.show();
-			
+			AlertDialog ad = builder.create();
+			ad.show();
+
 		}
 	};
-	
+
 	private OnTouchListener functionOnTouch = new OnTouchListener() {
-		
+
 		@Override
 		public boolean onTouch(View v, MotionEvent event) {
 			Button temp = (Button) v;
 			ColorDrawable d = (ColorDrawable) temp.getBackground();
 			switch(event.getAction()){
-		    case MotionEvent.ACTION_DOWN:
-		        temp.setTextColor(d.getColor());
-		        break;
+			case MotionEvent.ACTION_DOWN:
+				temp.setTextColor(d.getColor());
+				break;
 
-		    case MotionEvent.ACTION_MOVE:
-		    	break;
+			case MotionEvent.ACTION_MOVE:
+				break;
 
-		    case MotionEvent.ACTION_UP:
-		    	temp.setTextColor(Color.WHITE);
+			case MotionEvent.ACTION_UP:
+				temp.setTextColor(Color.WHITE);
 
-		    	float x = Math.abs(event.getX()),y = Math.abs(event.getY());
-		    	double dist = Math.sqrt( Math.pow(x, 2) + Math.pow(y, 2) );
-		    	if (dist < 300) {
-		    		String funct = temp.getText() + "";
+				float x = Math.abs(event.getX()),y = Math.abs(event.getY());
+				double dist = Math.sqrt( Math.pow(x, 2) + Math.pow(y, 2) );
+				if (dist < 300) {
+					String funct = temp.getText() + "";
 
-		    		AlertDialog.Builder builder = new AlertDialog.Builder(Game.this);
-		    		builder.setTitle("Action performed: " + funct)
-		    			.setMessage(dist + "");
-		    		builder.setPositiveButton("OK", null);
+					AlertDialog.Builder builder = new AlertDialog.Builder(Game.this);
+					builder.setTitle("Action performed: " + funct)
+					.setMessage(dist + "");
+					builder.setPositiveButton("OK", null);
 
-		    		AlertDialog ad = builder.create();
-		    		ad.show();
-		    	}
-		    	break;
+					AlertDialog ad = builder.create();
+					ad.show();
+				}
+				break;
 			}
 			return true;
 		}
 	};
-	
+
 	private void createHands() {
 		playerButtons = new ArrayList<Button>();
-		
-		
-		for (int i = 0x7f090001; i <= 0x7f09000e; i++) {
-			playerButtons.add((Button)findViewById(i));
-		}
+		bot3Tiles = new ArrayList<Button>();
+
+		playerButtons.add((Button)findViewById(R.id.playerTile0));
+		playerButtons.add((Button)findViewById(R.id.playerTile1));
+		playerButtons.add((Button)findViewById(R.id.playerTile2));
+		playerButtons.add((Button)findViewById(R.id.playerTile3));
+		playerButtons.add((Button)findViewById(R.id.playerTile4));
+		playerButtons.add((Button)findViewById(R.id.playerTile5));
+		playerButtons.add((Button)findViewById(R.id.playerTile6));
+		playerButtons.add((Button)findViewById(R.id.playerTile7));
+		playerButtons.add((Button)findViewById(R.id.playerTile8));
+		playerButtons.add((Button)findViewById(R.id.playerTile9));
+		playerButtons.add((Button)findViewById(R.id.playerTile10));
+		playerButtons.add((Button)findViewById(R.id.playerTile11));
+		playerButtons.add((Button)findViewById(R.id.playerTile12));
+
 		for (int i = 0; i <= 13; i++) {
 			playerHand.add(deck.draw());
 		}
@@ -122,20 +133,48 @@ public class Game extends Activity {
 			tempB.setOnClickListener(suit_valueListener);
 			//buttons.get(i).setOnClickListener(colorListener);		
 		}
+
+		bot3Tiles.add((Button)findViewById(R.id.botTile3_0));
+		bot3Tiles.add((Button)findViewById(R.id.botTile3_1));
+		bot3Tiles.add((Button)findViewById(R.id.botTile3_2));
+		bot3Tiles.add((Button)findViewById(R.id.botTile3_3));
+		bot3Tiles.add((Button)findViewById(R.id.botTile3_4));
+		bot3Tiles.add((Button)findViewById(R.id.botTile3_5));
+		bot3Tiles.add((Button)findViewById(R.id.botTile3_6));
+		bot3Tiles.add((Button)findViewById(R.id.botTile3_7));
+		bot3Tiles.add((Button)findViewById(R.id.botTile3_8));
+		bot3Tiles.add((Button)findViewById(R.id.botTile3_9));
+		bot3Tiles.add((Button)findViewById(R.id.botTile3_10));
+		bot3Tiles.add((Button)findViewById(R.id.botTile3_11));
+		bot3Tiles.add((Button)findViewById(R.id.botTile3_12));
 		
+		for (int i = 0; i <= 12; i++) {
+			Button temp = bot3Tiles.get(i);
+			if (i % 2 == 0) {
+				temp.setBackgroundColor(Color.MAGENTA);
+				temp.setText("2");
+			}
+			else {
+				temp.setBackgroundColor(Color.BLUE);
+				temp.setText("1");
+			}
+			bot3Tiles.add(temp);
+
+		}
+
 		Button eatButton = (Button) findViewById(R.id.eatButton);
 		eatButton.setOnTouchListener(functionOnTouch);
-		
+
 		Button doubleButton = (Button) findViewById(R.id.doubleButton);
 		doubleButton.setOnTouchListener(functionOnTouch);
-		
+
 		Button tripleButton = (Button) findViewById(R.id.tripleButton);
 		tripleButton.setOnTouchListener(functionOnTouch);
-		
+
 		Button winButton = (Button) findViewById(R.id.winButton);
 		winButton.setOnTouchListener(functionOnTouch);
 	}
-	
+
 	private int randomPlayer() {
 		Random r = new Random();
 		return r.nextInt(4);
